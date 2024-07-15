c1b0f342d6c0dd8d2599b920bd95fa671d55798e
sorav <sourav@centersource.online>
2024-07-15 14:24:49 +0530

    DEMO

---
+++ src/GWT/Wood.java
@@ -0,0 +1,17 @@
+package GWT;
+
+import org.openqa.selenium.WebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Wood {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+	WebDriver driver=	WebDriverManager.chromeDriver().create();
+	
+
+}
+}
+
--- src/SecondClass.java
+++
@@ -1,9 +0,0 @@
-public class SecondClass {
-
-    public static void main(String[] args) {
-        System.out.println("Hello RAM");
-        System.out.print("Hello SAM");
-
-
-    }
-}
